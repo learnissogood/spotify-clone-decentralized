3dd30d4e69a3c26b08ccdd4913c45ff976733f31
juanchi097 <juanchi097@gmail.com>
2022-05-04 20:17:49 -0300

    First UI Components Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Album from './pages/Album';
 import './App.css';
+import { Layout } from "antd";
+import Spotify from "./images/Spotify.png";
+import { DownCircleOutlined, SearchOutlined } from '@ant-design/icons';
+
+const { Footer, Sider, Content } = Layout;
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
-  )
+  return (
+    <Layout>
+      <Sider width={300} className="sideBar">
+        <img src={Spotify} alt="Logo" className="logo"></img>
+        <div className='searchBar'>
+          <span> Search </span>
+          <SearchOutlined style={{ fontSize: "30px" }}/>
+        </div>
+        <Link to="/">
+          <p style={{ color: "#1DB954" }}> Home </p>
+        </Link>
+        <p> Your Music </p>
+        <div className='recentPlayed'>
+          <p className='recentTitle'> RECENTLY PLAYED </p>
+          <div className='install'>
+            <span> Install App </span>
+            <DownCircleOutlined style={{ fontSize: "30px" }}/>
+          </div>
+        </div>
+      </Sider>
+      <Content className='contentWindow'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/album" element={<Album />} />
+        </Routes>
+      </Content>
+      <Sider>right sidebar</Sider>
+    </Layout>
+  );
 };
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
+import {  Tabs } from "antd";
+import { library } from '../helpers/albumList';
+
+const { TabPane } = Tabs;
 
 const Home = () => {
 
 return(
   <>
-  <div className="container">
-     <Link to="/album" className="link">Album Page</Link>
-  </div>
+  <Tabs defaultActiveKey="1" centered>
+    <TabPane tab="FEATURED" key="1">
+      <h1 className='featuredTitle'>Today Is The Day</h1>
+      <div className='albums'>
+        {library.map((e) => (
+          <Link to="/album" state={e} className="albumSelection">
+            <img
+              src={e.image}
+              alt="bull"
+              style={{ width: "150px", marginBottom: "10px" }}
+            >
+            </img>
+            <p>{e.title}</p>
+          </Link>
+        ))}
+      </div>
+    </TabPane>
+    <TabPane tab="GENRES & MOODS" key="2">
+      Content of Tab Pane 2
+    </TabPane>
+    <TabPane tab="NEW RELEASES" key="3">
+      Content of Tab Pane 3
+    </TabPane>
+  </Tabs>
   </>
 )
 }
